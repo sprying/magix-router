cd1b02730dc4fdf38695508842a2806ef559c7fd
yingchun.fyc <yingchun.fyc@alibaba-inc.com>
2019-05-15 22:07:43 +0800

    支持新的mode

--- src/history/abstract.js
+++ src/history/abstract.js
@@ -10,7 +10,7 @@ export class AbstractHistory extends History {
   constructor (router: Router, base: ?string) {
     super(router, base)
     this.stack = []
-    this.index = -1
+    this.index = 0
   }
 
   push (location: RawLocation, onComplete?: Function, onAbort?: Function) {
